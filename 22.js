65872e8960321a55f1cdd95e2500b889e3d745fe
nik0rai <nikolay-sviridov@inbox.ru>
2023-01-13 14:53:01 +0300

    Update 22.js

--- 22.js
+++ 22.js
@@ -1,28 +1,30 @@
 class Point {
     x;
     y;
-    constructor(x,y){
+    z;
+    constructor(x,y,z){
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 }
 
 const n = 5;
-let plane_2d = new Array(n);
+let plane = new Array(n);
 
 for(var i = 0; i < n; i++){
-    plane_2d[i] = new Point(Math.floor(Math.random() * (10 - (-10)) + (-10)), 
+    plane[i] = new Point(Math.floor(Math.random() * (10 - (-10)) + (-10)), 
                             Math.floor(Math.random() * (10 - (-10)) + (-10)))
 }
 
 console.log("array of points:")
-plane_2d.forEach(x=>{
+plane.forEach(x=>{
     console.log(x)
 })
 
 var res = false;
 for(var i = 0; i<n; i++){
-    if(i.x === 0 && i.y === 0){
+    if(plane[i].x === 0 && plane[i].y === 0 && plane[i].z === 0){
         res = true
         break;
     }
